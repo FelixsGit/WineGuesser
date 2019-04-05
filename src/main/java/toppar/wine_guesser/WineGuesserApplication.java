138ea9a87e06b47fdac2cf3d3f6661722c4d5dd6
Felix Toppar <toppar@kth.se>
2019-04-05 20:07:23 +0200

    First commit

---
+++ src/main/java/toppar/wine_guesser/WineGuesserApplication.java
@@ -0,0 +1,13 @@
+package toppar.wine_guesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineGuesserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineGuesserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toppar/wine_guesser/WineGuesserApplicationTests.java
@@ -0,0 +1,16 @@
+package toppar.wine_guesser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WineGuesserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
