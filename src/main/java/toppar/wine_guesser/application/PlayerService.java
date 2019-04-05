f6b6b6926d32627ad0e25d0a34d70eb1aa1e45a0
FelixsGit <toppar@kth.se>
2019-04-06 01:25:00 +0200

    Fix minor css/getmapping bug

--- src/main/java/toppar/wine_guesser/application/PlayerService.java
+++ src/main/java/toppar/wine_guesser/application/PlayerService.java
@@ -1,4 +1,8 @@
 package toppar.wine_guesser.application;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerService {
+
 }
--- src/main/java/toppar/wine_guesser/presentation/app/ApplicationController.java
+++ src/main/java/toppar/wine_guesser/presentation/app/ApplicationController.java
@@ -1,14 +1,19 @@
 package toppar.wine_guesser.presentation.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import toppar.wine_guesser.application.PlayerService;
 
 @Controller
 public class ApplicationController {
 
-    @GetMapping()
-    public String getHome(){
-        return "/home";
+    @Autowired
+    PlayerService playerService;
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
     }
 
 
